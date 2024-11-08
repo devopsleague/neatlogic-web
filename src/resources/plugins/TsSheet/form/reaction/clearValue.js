4b81bc5100a0bb4f05c02f8a294f82c67f60b191
dengbf <dengbf@aliyun.com>
2024-11-08 14:28:28 +0800

    [关联] #[1284826911440896]表单管理-表格选择扩展属性的清空值联动没有生效 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1284826911440896

--- src/resources/plugins/TsSheet/form/reaction/clearValue.js
+++ src/resources/plugins/TsSheet/form/reaction/clearValue.js
@@ -1,6 +1,7 @@
 export default ({result, view}) => {
   if (result && !view.$utils.isEmpty(view.formData[view.formItem.uuid])) {
-    view.$set(view.formData, view.formItem.uuid, null);
+    //清空当前组件数据
+    view.setValue(null);
   }
   view.addExecuteCount('clearValue');
 };
